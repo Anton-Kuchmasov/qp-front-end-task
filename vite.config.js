fa7eba297dfe1ac5ca2f2d674937dfba2119c932
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-02-27 22:23:51 +0200

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://anton-kuchmasov.github.io/qp-front-end-task/',
   plugins: [react()],
 })
